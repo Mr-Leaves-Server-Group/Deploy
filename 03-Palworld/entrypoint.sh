626d393f74c760d379aff120b01b090866d13086
Ken Ren <81537377+KenRen98@users.noreply.github.com>
2026-07-28 19:08:36 +0800

    Short version

--- 03-Palworld/entrypoint.sh
+++ 03-Palworld/entrypoint.sh
@@ -74,7 +74,7 @@ fi
     done
 ) &
 
-export RESTAPI_PORT={{REST_API_PORT}} ADMIN_PASSWORD={{ADMIN_PASSWORD}} WINEDLLOVERRIDES="dwmapi=n,b"
+export WINEDLLOVERRIDES="dwmapi=n,b"
 
 # 替换启动变量占位符
 MODIFIED_STARTUP=$(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
