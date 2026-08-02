126b9997a7f6566ff59f4572b51bd0f72bd37d1f
Ken Ren <81537377+KenRen98@users.noreply.github.com>
2026-08-03 07:41:55 +0800

    写法错误

--- 03-Palworld/entrypoint.sh
+++ 03-Palworld/entrypoint.sh
@@ -75,7 +75,7 @@ fi
 ) &
 
 export WINEDLLOVERRIDES="dwmapi=n,b"
-export RESTAPI_PORT={{REST_API_PORT}} ADMIN_PASSWORD={{ADMIN_PASSWORD}}
+export RESTAPI_PORT=${REST_API_PORT} ADMIN_PASSWORD=${ADMIN_PASSWORD}
 
 # 替换启动变量占位符
 MODIFIED_STARTUP=$(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
